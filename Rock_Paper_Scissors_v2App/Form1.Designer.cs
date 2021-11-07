bad571ff783ab10d1c09e3adccb462fc868082a5
KatyaDremliuh <nerona.91@gmail.com>
2021-11-07 23:19:07 +0300

    New sizes

--- Rock_Paper_Scissors_v2App/Form1.Designer.cs
+++ Rock_Paper_Scissors_v2App/Form1.Designer.cs
@@ -52,68 +52,68 @@ namespace Rock_Paper_Scissors_v2App
             // 
             // labelBorderCompRock
             // 
-            this.labelBorderCompRock.BackColor = System.Drawing.SystemColors.Control;
-            this.labelBorderCompRock.Location = new System.Drawing.Point(18, 60);
+            this.labelBorderCompRock.BackColor = System.Drawing.SystemColors.Info;
+            this.labelBorderCompRock.Location = new System.Drawing.Point(50, 50);
             this.labelBorderCompRock.Name = "labelBorderCompRock";
-            this.labelBorderCompRock.Size = new System.Drawing.Size(158, 158);
+            this.labelBorderCompRock.Size = new System.Drawing.Size(200, 200);
             this.labelBorderCompRock.TabIndex = 0;
             // 
             // labelBorderUserRock
             // 
-            this.labelBorderUserRock.BackColor = System.Drawing.SystemColors.Control;
-            this.labelBorderUserRock.Location = new System.Drawing.Point(20, 62);
+            this.labelBorderUserRock.BackColor = System.Drawing.SystemColors.Info;
+            this.labelBorderUserRock.Location = new System.Drawing.Point(65, 65);
             this.labelBorderUserRock.Name = "labelBorderUserRock";
-            this.labelBorderUserRock.Size = new System.Drawing.Size(154, 154);
+            this.labelBorderUserRock.Size = new System.Drawing.Size(170, 170);
             this.labelBorderUserRock.TabIndex = 1;
             // 
             // labelBorderUserPaper
             // 
-            this.labelBorderUserPaper.BackColor = System.Drawing.SystemColors.Control;
-            this.labelBorderUserPaper.Location = new System.Drawing.Point(200, 62);
+            this.labelBorderUserPaper.BackColor = System.Drawing.SystemColors.Info;
+            this.labelBorderUserPaper.Location = new System.Drawing.Point(365, 65);
             this.labelBorderUserPaper.Name = "labelBorderUserPaper";
-            this.labelBorderUserPaper.Size = new System.Drawing.Size(154, 154);
+            this.labelBorderUserPaper.Size = new System.Drawing.Size(170, 170);
             this.labelBorderUserPaper.TabIndex = 4;
             // 
             // labelBorderCompPaper
             // 
-            this.labelBorderCompPaper.BackColor = System.Drawing.SystemColors.Control;
-            this.labelBorderCompPaper.Location = new System.Drawing.Point(198, 60);
+            this.labelBorderCompPaper.BackColor = System.Drawing.SystemColors.Info;
+            this.labelBorderCompPaper.Location = new System.Drawing.Point(350, 50);
             this.labelBorderCompPaper.Name = "labelBorderCompPaper";
-            this.labelBorderCompPaper.Size = new System.Drawing.Size(158, 158);
+            this.labelBorderCompPaper.Size = new System.Drawing.Size(200, 200);
             this.labelBorderCompPaper.TabIndex = 3;
             // 
             // labelBorderUserScissors
             // 
-            this.labelBorderUserScissors.BackColor = System.Drawing.SystemColors.Control;
-            this.labelBorderUserScissors.Location = new System.Drawing.Point(380, 62);
+            this.labelBorderUserScissors.BackColor = System.Drawing.SystemColors.Info;
+            this.labelBorderUserScissors.Location = new System.Drawing.Point(650, 50);
             this.labelBorderUserScissors.Name = "labelBorderUserScissors";
-            this.labelBorderUserScissors.Size = new System.Drawing.Size(154, 154);
+            this.labelBorderUserScissors.Size = new System.Drawing.Size(200, 200);
             this.labelBorderUserScissors.TabIndex = 7;
             // 
             // labelBorderCompScissors
             // 
-            this.labelBorderCompScissors.BackColor = System.Drawing.SystemColors.Control;
-            this.labelBorderCompScissors.Location = new System.Drawing.Point(378, 60);
+            this.labelBorderCompScissors.BackColor = System.Drawing.SystemColors.Info;
+            this.labelBorderCompScissors.Location = new System.Drawing.Point(665, 65);
             this.labelBorderCompScissors.Name = "labelBorderCompScissors";
-            this.labelBorderCompScissors.Size = new System.Drawing.Size(158, 158);
+            this.labelBorderCompScissors.Size = new System.Drawing.Size(170, 170);
             this.labelBorderCompScissors.TabIndex = 6;
             // 
             // labelComputerName
             // 
-            this.labelComputerName.Font = new System.Drawing.Font("Monotype Corsiva", 19F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelComputerName.Location = new System.Drawing.Point(18, 260);
+            this.labelComputerName.Font = new System.Drawing.Font("Arial Black", 19F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelComputerName.Location = new System.Drawing.Point(35, 400);
             this.labelComputerName.Name = "labelComputerName";
-            this.labelComputerName.Size = new System.Drawing.Size(150, 45);
+            this.labelComputerName.Size = new System.Drawing.Size(200, 90);
             this.labelComputerName.TabIndex = 9;
             this.labelComputerName.Text = "Computer";
             this.labelComputerName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // labelUserName
             // 
-            this.labelUserName.Font = new System.Drawing.Font("Monotype Corsiva", 19F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelUserName.Location = new System.Drawing.Point(378, 260);
+            this.labelUserName.Font = new System.Drawing.Font("Arial Black", 19F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUserName.Location = new System.Drawing.Point(660, 400);
             this.labelUserName.Name = "labelUserName";
-            this.labelUserName.Size = new System.Drawing.Size(150, 45);
+            this.labelUserName.Size = new System.Drawing.Size(200, 90);
             this.labelUserName.TabIndex = 10;
             this.labelUserName.Text = "User";
             this.labelUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -122,9 +122,9 @@ namespace Rock_Paper_Scissors_v2App
             // labelScore
             // 
             this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
-            this.labelScore.Location = new System.Drawing.Point(198, 260);
+            this.labelScore.Location = new System.Drawing.Point(340, 400);
             this.labelScore.Name = "labelScore";
-            this.labelScore.Size = new System.Drawing.Size(158, 45);
+            this.labelScore.Size = new System.Drawing.Size(220, 90);
             this.labelScore.TabIndex = 11;
             this.labelScore.Text = "0 : 0";
             this.labelScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -132,9 +132,9 @@ namespace Rock_Paper_Scissors_v2App
             // buttonNext
             // 
             this.buttonNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.buttonNext.Location = new System.Drawing.Point(420, 320);
+            this.buttonNext.Location = new System.Drawing.Point(390, 495);
             this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(100, 25);
+            this.buttonNext.Size = new System.Drawing.Size(120, 40);
             this.buttonNext.TabIndex = 12;
             this.buttonNext.Text = "Play more";
             this.buttonNext.UseVisualStyleBackColor = true;
@@ -142,10 +142,10 @@ namespace Rock_Paper_Scissors_v2App
             // 
             // labelInfoTable
             // 
-            this.labelInfoTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.labelInfoTable.Location = new System.Drawing.Point(20, 20);
+            this.labelInfoTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic);
+            this.labelInfoTable.Location = new System.Drawing.Point(53, 20);
             this.labelInfoTable.Name = "labelInfoTable";
-            this.labelInfoTable.Size = new System.Drawing.Size(510, 25);
+            this.labelInfoTable.Size = new System.Drawing.Size(797, 30);
             this.labelInfoTable.TabIndex = 13;
             this.labelInfoTable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -153,10 +153,10 @@ namespace Rock_Paper_Scissors_v2App
             // 
             this.txtReadName.BackColor = System.Drawing.SystemColors.Control;
             this.txtReadName.Enabled = false;
-            this.txtReadName.Font = new System.Drawing.Font("Monotype Corsiva", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtReadName.Location = new System.Drawing.Point(384, 271);
+            this.txtReadName.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtReadName.Location = new System.Drawing.Point(645, 419);
             this.txtReadName.Name = "txtReadName";
-            this.txtReadName.Size = new System.Drawing.Size(150, 34);
+            this.txtReadName.Size = new System.Drawing.Size(190, 53);
             this.txtReadName.TabIndex = 14;
             this.txtReadName.Text = "Player";
             this.txtReadName.Visible = false;
@@ -165,9 +165,9 @@ namespace Rock_Paper_Scissors_v2App
             // imgRock
             // 
             this.imgRock.Image = ((System.Drawing.Image)(resources.GetObject("imgRock.Image")));
-            this.imgRock.Location = new System.Drawing.Point(22, 64);
+            this.imgRock.Location = new System.Drawing.Point(80, 80);
             this.imgRock.Name = "imgRock";
-            this.imgRock.Size = new System.Drawing.Size(150, 150);
+            this.imgRock.Size = new System.Drawing.Size(140, 140);
             this.imgRock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgRock.TabIndex = 2;
             this.imgRock.TabStop = false;
@@ -176,9 +176,9 @@ namespace Rock_Paper_Scissors_v2App
             // imgPaper
             // 
             this.imgPaper.Image = ((System.Drawing.Image)(resources.GetObject("imgPaper.Image")));
-            this.imgPaper.Location = new System.Drawing.Point(202, 64);
+            this.imgPaper.Location = new System.Drawing.Point(680, 80);
             this.imgPaper.Name = "imgPaper";
-            this.imgPaper.Size = new System.Drawing.Size(150, 150);
+            this.imgPaper.Size = new System.Drawing.Size(140, 140);
             this.imgPaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgPaper.TabIndex = 5;
             this.imgPaper.TabStop = false;
@@ -187,9 +187,9 @@ namespace Rock_Paper_Scissors_v2App
             // imgScissor
             // 
             this.imgScissor.Image = ((System.Drawing.Image)(resources.GetObject("imgScissor.Image")));
-            this.imgScissor.Location = new System.Drawing.Point(382, 64);
+            this.imgScissor.Location = new System.Drawing.Point(380, 80);
             this.imgScissor.Name = "imgScissor";
-            this.imgScissor.Size = new System.Drawing.Size(150, 150);
+            this.imgScissor.Size = new System.Drawing.Size(140, 140);
             this.imgScissor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imgScissor.TabIndex = 8;
             this.imgScissor.TabStop = false;
@@ -200,7 +200,8 @@ namespace Rock_Paper_Scissors_v2App
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
-            this.ClientSize = new System.Drawing.Size(542, 353);
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.imgPaper);
             this.Controls.Add(this.txtReadName);
             this.Controls.Add(this.labelInfoTable);
             this.Controls.Add(this.buttonNext);
@@ -208,14 +209,13 @@ namespace Rock_Paper_Scissors_v2App
             this.Controls.Add(this.labelUserName);
             this.Controls.Add(this.labelComputerName);
             this.Controls.Add(this.imgScissor);
-            this.Controls.Add(this.labelBorderUserScissors);
             this.Controls.Add(this.labelBorderCompScissors);
-            this.Controls.Add(this.imgPaper);
             this.Controls.Add(this.labelBorderUserPaper);
             this.Controls.Add(this.labelBorderCompPaper);
             this.Controls.Add(this.imgRock);
             this.Controls.Add(this.labelBorderUserRock);
             this.Controls.Add(this.labelBorderCompRock);
+            this.Controls.Add(this.labelBorderUserScissors);
             this.Name = "Form1";
             this.Text = "Rock-Paper-Scissors_v2";
             ((System.ComponentModel.ISupportInitialize)(this.imgRock)).EndInit();
